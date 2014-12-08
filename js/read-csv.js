3f3bf78e7658007fc438862e05fa2b5f272e2faf
msriniva <macsdev@gmail.com>
2014-12-07 19:35:50 -0800

    Start tracking your money

---
+++ js/read-csv.js
@@ -0,0 +1,128 @@
+var restaurants = {'chain': 2, 'ale': 1, 'jims': 1, 'carrabbas': 1, 'roy': 3, 'asian': 1, 'barbecue': 2, 'captain': 1, 'atlanta': 2, 'showmars': 1, 'canes': 1, 'farmi ngton': 1, 'shack': 2, 'burlington': 1, 'maryland': 1, 'southwest': 1, 'gar den': 1, 'deli': 3, 'tacos': 1, 'innout': 1, 'jack': 1, 'wilbraham': 1, 'a ngus': 1, 'chevys': 1, 'brewery': 1, 'p': 1, 'chuckarama': 1, 'steakhouse': 8, 'subway': 1, 'sidewalk': 1, 'round': 2, 'carls': 1, 'edina': 1, 'bravo ': 1, 'quaker': 1, 'rubios': 1, 'burger': 4, 'street': 1, 'spaghetti': 2, 'casual': 2, 'camilles': 1, 'blue': 1, 'cheeses': 1, 'checkers': 2, 'sub': 1, 'chicago': 2, 'fingers': 2, 'roadhouse': 2, 'capital': 1, 'new': 1, 'ro ys': 1, 'hero': 1, 'lee': 2, 'changs': 1, 'blimpie': 1, 'china': 1, 'panca ke': 1, 'mex': 1, 'box': 1, 'boy': 1, 'buca': 1, 'dublin': 1, 'hooters': 1 , 'umami': 1, 'california': 8, 'bob': 1, 'popeyes': 1, 'saladworks': 1, 'g olden': 3, 'county': 2, 'eegees': 1, 'market': 2, 'donuts': 1, 'hungry': 1, 'sports': 1, 'crab': 1, 'dairy': 1, 'milford': 1, 'company': 3, 'downtown ': 1, 'connecticut': 2, 'olive': 1, 'benihana': 1, 'circle': 1, 'tampa': 4, 'grill': 15, 'naugles': 1, 'f': 1, 'arbys': 1, 'callenders': 1, 'landrys' : 1, 'del': 1, 'mr': 1, 'sizzler': 1, 'caf': 1, 'claim': 1, 'crazy': 1, 'hamburger': 3, 'loco': 1, 'states': 1, 'donatos': 1, 'bones': 1, 'gameworks ': 1, 'hamburgers': 1, 'united': 1, 'mazzios': 1, 'hot': 1, 'elephant': 1, 'browns': 1, 'stir': 1, 'pizza': 17, 'sandwich': 1, 'max': 1, 'scottsdale' : 1, 'dominos': 1, 'taco': 9, 'plano': 2, 'choice': 1, 'guys': 1, 'milos': 1, 'schlotzskys': 1, 'joint': 1, 'charleys': 1, 'corral': 1, 'eatn': 1, 'bakery': 2, 'daves': 1, 'bakers': 1, 'wine': 1, 'houston': 2, 'damons': 1, 'mexican': 4, 'brio': 2, 'shake': 1, 'cold': 1, 'subs': 4, 'group': 1, 'l l': 1, 'caesars': 1, 'brook': 1, 'louisville': 1, 'denver': 1, 'valentinos' : 1, 'jasons': 1, 'farmer': 1, 'fuddruckers': 1, 'texas': 6, 'bonefish': 1, 'coffee': 1, 'bojangles': 1, 'potbelly': 1, 'portillos': 1, 'starbucks': 1 , 'aliso': 1, 'hall': 1, 'bread': 2, 'huddle': 1, 'chuck': 1, 'detroit': 2 , 'logans': 1, 'calabasas': 1, 'rock': 2, 'biscuits': 2, 'ledo': 1, 'el': 3, 'square': 1, 'dixie': 1, 'lubys': 1, 'house': 6, 'fish': 1, 'hard': 1, 'jerrys': 2, 'whataburger': 1, 'quiznos': 1, 'hardees': 1, 'cheddars': 1, 'sandy': 1, 'selmon': 1, 'oklahoma': 2, 'factory': 2, 'krispy': 1, 'ra': 1, 'ihop': 1, 'paradise': 2, 'wich': 1, 'bubba': 1, 'red': 3, 'bistro': 1, 'arizona': 2, 'millburn': 1, 'jumper': 1, 'grilled': 1, 'braums' : 1, 'york': 1, 'viejo': 1, 'tortilla': 1, 'florida': 13, 'pablos': 1, 'am erican': 1, 'hawaiian': 1, 'rainforest': 1, 'pot': 1, 'castle': 1, 'south': 2, 'tijuana': 1, 'lyons': 1, 'evans': 1, 'perkins': 1, 'kfc': 1, 'millers ': 1, 'city': 2, 'little': 2, 'long': 1, 'smashburger': 1, 'jamba': 1, 'st ation': 2, 'white': 1, 'john': 1, 'store': 1, 'park': 1, 'hut': 1, 'shanes ': 1, 'murphys': 1, 'baja': 1, 'hollywood': 1, 'king': 1, 'ben': 1, 'kreme ': 1, 'panda': 1, 'ginos': 2, 'boston': 1, 'pro': 1, 'steakburgers': 1, 'juice': 1, 'mongolian': 1, 'st': 1, 'border': 1, 'rax': 1, 'silvers': 1, 'c anton': 1, 'dining': 1, 'bonanza': 1, 'green': 2, 'maggianos': 1, 'cheeseca ke': 1, 'spangles': 1, 'montana': 1, 'seasons': 1, 'marie': 1, 'don': 1, 'rogers': 1, 'houstons': 1, 'frederick': 1, 'mcalisters': 1, 'mrs': 1, 'blac keyed': 1, 'saloon': 1, 'queen': 1, 'recipe': 1, 'coffeehouse': 1, 'chicken ': 6, 'ground': 1, 'moes': 1, 'freebirds': 1, 'bennigans': 1, 'cheeseburger ': 2, 'minnesota': 2, 'lube': 1, 'black': 1, 'spartanburg': 1, 'longhorn': 1, 'chipotle': 1, 'carolina': 2, 'di': 1, 'michigan': 1, 'express': 2, 'ma ddios': 1, 'fatburger': 1, 'famous': 3, 'cheeburger': 2, 'shoneys': 1, 'ds' : 1, 'prime': 1, 'bar': 3, 'burgerville': 1, 'fields': 1, 'lexington': 1, 'missouri': 2, 'skyline': 1, 'glendale': 1, 'ohio': 2, 'steak': 4, 'restaurants': 7, 'national': 2, 'noodles': 1, 'mcdonalds': 1, 'best': 1, 'chickfil a': 1, 'barbeque': 1, 'jr': 1, 'wienerschnitzel': 1, 'kitchen': 1, 'league' : 1, 'restaurant': 29, 'country': 1, 'romas': 1, 'pita': 1, 'jersey': 2, 'uno': 1, 'churchs': 1, 'sweet': 1, 'hobees': 1, 'twin': 1, 't ennessee': 2, 'village': 1, 'tony': 1, 'springs': 1, 'ermas': 1, 'busters': 1, 'mikes': 2, 'mitchells': 1, 'howard': 1, 'bikinis': 1, 'buffett': 1, 'buffets': 2, 'northborough': 1, 'beppo': 1, 'louisiana': 2, 'applebees': 1, 'zaxbys': 1, 'irvine': 2, 'dave': 1, 'penn': 1, 'chili': 2, 'ruby': 1, 'g eorgia': 4, 'maryville': 1, 'frozen': 1, 'bahama': 1, 'cracker': 1, 'kentucky': 2, 'n': 2, 'pollo': 1, 'wild': 1, 'robin': 1, 'bertuccis': 1, 'freddy s': 1, 'sushi': 1, 'ii': 1, 'rio': 1, 'seattle': 1, 'rib': 1, 'fame': 1, 'cafe': 4, 'italy': 1, 'chilis': 1, 'swensons': 1, 'cabana': 1, 'port': 1, 'food': 6, 'macaroni': 1, 'tomatoes': 1, 'peaks': 1, 'arctic': 1, 'dennys' : 1, 'qdoba': 1, 'fridays': 1, 'louis': 1, 'culvers': 1, 'lees': 1, 'vermont': 1, 'burrito': 2, 'smokey': 1, 'charlie': 1, 'ranch': 1, 'eatzis': 1, 'breeze': 1, 'cicis': 1, 'howies': 1, 'kilt': 1, 'winstonsalem': 1, 'houlih ans': 1, 'world': 1, 'doral': 1, 'illinois': 1, 'godfathers': 1, 'wei': 1, 'bell': 1, 'carrows': 1, 'dickeys': 1, 'east': 1, 'tuesday': 1, 'sonic': 1 , 'ponderosa': 1, 'big': 1, 'mayo': 1, 'gwinnett': 1, 'flemings': 1, 'carr ollton': 1, 'fatz': 1, 'wings': 1, 'tico': 1, 'melting': 1, 'ocharleys': 1, 'orlando': 5, '52': 1, 'chick': 1, 'works': 1, 'chico': 1, 'italian': 3, 'copelands': 1, 'old': 2, 'cinnabon': 1, 'ruths': 1, 'lebanon': 1, 'coney' : 1, 'lobster': 1, 'table': 1, 'petes': 1, 'redstone': 1, 'tgi': 1, 'tote' : 1, 'romanos': 1, 'wendys': 1, 'pei': 1, 'rockets': 1, 'pea': 1, 'columbus': 1, 'football': 2, 'dunkin': 1, 'annes': 1, 'stone': 1, 'diner': 1, 'is land': 1, 'fazolis': 1, 'shrimp': 1, 'gattis': 1, 'gump': 1, 'swensens': 1, 'cocos': 1, 'jimmy': 2, 'colorado': 2, 'auntie': 1, 'souplantation': 1, 'johns': 3, 'washington': 1, 'chronic': 1, 'tilted': 1, 'bjs': 1, 'miamidade ': 1, 'bueno': 1, 'custard': 1, 'sneaky': 1, 'creamery': 1, 'cantina': 1, 'lone': 1, 'fast': 6, 'north': 1, 'jacks': 1, 'robeks': 1, 'boys': 1, 'unc le': 1, 'krystal': 1, 'waffle': 1, 'raising': 1, 'runza': 1, 'planet': 1, 'flats': 1, 'carinos': 1, 'johnny': 1, 'pie': 1, 'minneapolis': 1, 'sticky' : 1, 'arabi': 1, 'inn': 2, 'aw': 1, 'johnsons': 1, 'norcross': 1, 'pit': 2 , 'carvel': 1, 'friendlys': 1, 'oak': 1, 'seattles': 1, 'sbarro': 1, 'huho t': 1, 'champps': 1, 'star': 1, 'kansas': 1, 'papa': 3, 'outback': 1, 'pan era': 1, 'massachusetts': 3, 'chris': 1, 'drivein': 1, 'grille': 1, 'barrel ': 1, 'e': 1, 'vics': 1, 'fresh': 4, 'buffalo': 1, 'original': 2, 'selmons': 1, 'cuisine': 1, 'tandoor': 1, 'taste': 1, 'rubio' : 1, 'shere': 1, 'surati': 1, 'muzita': 1};
+var gas = {'stores': 3, 'farms': 2, 'wawa': 1, 'citgo': 1, 'group': 1, 'cumberland':  1, 'gulf': 2, 'royal': 3, 'amoco': 1, 'petro': 1, 'dairy': 1, 'fuel': 1, 'holiday': 1, 'conoco': 1, 'sonic': 1, 'food': 1, 'express': 1, 'sunoco': 1, 'exxon': 2, 'spinx': 1, 'dodges': 1, 'page': 4, 'j': 1, 'england': 1, 'chevron': 1, 'energy': 4, 'gomart': 1, 'quebec': 1, 'atlantic': 2, 'sprint ': 1, 'canada': 5, 'hess': 1, 'pioneer': 1, 'kum': 1, 'does': 4, 'dutch': 2, 'new': 1, 'domo': 1, 'racetrac': 1, 'stationstores': 1, 'bp': 3, 'bucee s': 1, 'tire': 1, '7eleven': 2, 'country': 1, 'union': 1, 'sinclair': 1, ' family': 1, 'america': 1, '66': 1, 'mobil': 1, 'city': 1, 'ontario': 4, 't exaco': 2, 'quickchek': 1, 'flying': 1, 'petroleum': 3, 'imperial': 1, 'olc o': 1, 'maverik': 1, 'markets': 1, 'shell': 3, 'supertest': 1, 'murphy': 1,  'gas': 1, 'irving': 2, 'getty': 1, 'kwik': 1, 'delta': 1, 'pilot': 2, 'c anadian': 1, 'gasoline': 2, 'valero': 1, 'quiktrip': 1, 'loves': 1, 'travel centers': 1, 'kroger': 1, 'speedway': 1, 'foods': 1, 'exist': 4, 'suncor': 2, 'trip': 1, 'lukoil': 1, 'wilson': 1, 'corporation': 5, 'phillips': 1, ' marathon': 1, 'rutters': 1, 'superamerica': 1, 'oil': 9, 'highs': 1, '76': 1, 'ultramar': 1, 'arco': 1, 'esso': 2, 'sheetz': 1, 'mohawk': 1, 'pemex':  1, 'petrofina': 1, 'husky': 1};
+var grocery = {'walmart':1, 'vons':1, 'grocery':1, 'trader':1, 'cvs':1, 'miramar':1, 'namaste':1, 'wal': 1, 'mart':1};
+var online = {'amazon':1, 'groupon':1};
+var travel = {'usairways':1};
+var utilities = {'twc': 1, 'at&t':1};
+var apparel = {'clothing':1, 'haggar':1, 'gap':1, 'levis':1, 'outlet':1, 'tommy':1, 'bahama':1};
+var entertainment = {'edwards':1, 'amc':1, 'netflix':1};
+var stopwords = ['a', 'about', 'above', 'across', 'after', 'afterwards', 'again', 'again st', 'all', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'am', 'among', 'amongst', 'amoungst', 'amount', 'an', 'and', 'another', 'any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere', 'are', 'around', 'as', 'at', 'back', 'be', 'became', 'because', 'become', 'becomes', 'becoming', 'been', 'before', 'beforehand', 'behind', 'being', 'below', 'beside', 'besides', 'between', 'beyond', 'bill', 'both', 'bott om', 'but', 'by', 'call', 'can', 'cannot', 'cant', 'co', 'computer', 'con', 'could', 'couldnt', 'cry', 'de', 'describe', 'detail', 'do', 'done' , 'down', 'due', 'during', 'each', 'eg', 'eight', 'either', 'eleven', 'else', 'elsewhere', 'empty', 'enough', 'etc', 'even', 'ever', 'every', 'everyone', 'everything', 'everywhere', 'except', 'few', 'fifteen', 'fify', 'fill', 'find', 'fire', 'first', 'five', 'for', 'former', 'formerly', 'forty', 'found', 'four', 'from', 'front', 'full', 'further', 'get', 'give', 'go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'however', 'hundred', 'i', 'ie', 'if', 'in', 'inc', 'indeed', 'interest', 'into', 'is', 'it', 'its', 'itself', 'keep', 'last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made', 'many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine', 'more', 'moreover', 'most', 'mostly', 'move', 'much', 'must', 'my', 'myself', 'name', 'namely ', 'neither', 'never', 'nevertheless', 'next', 'nine', 'no', 'nobody', 'none', 'noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of', 'off', 'often', 'on', 'once', 'one', 'only', 'onto', 'or', 'other', 'others', 'otherwise', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'part', 'per', 'perhaps', 'please', 'put', 'rather', 're', 'same', 'see', 'seem' , 'seemed', 'seeming', 'seems', 'serious', 'several', 'she', 'should', 'show', 'side', 'since', 'sincere', 'six', 'sixty', 'so', 'some', 'someho w', 'someone', 'something', 'sometime', 'sometimes', 'somewhere', 'still', 'such', 'system', 'take', 'ten', 'than', 'that', 'the', 'their', 'them ', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 'therefore', 'therein', 'thereupon', 'these', 'they', 'thick', 'thin', 'third ', 'this', 'those', 'though', 'three', 'through', 'throughout', 'thr', 'thus', 'to', 'together', 'too', 'top', 'toward', 'towards', 'twelve', 'twenty', 'two', 'un', 'under', 'until', 'up', 'upon', 'us', 'very', 'via', 'was', 'we', 'well', 'were', 'what', 'whatever', 'when', 'whence', 'whenever', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', 'where upon', 'wherever', 'whether', 'which', 'while', 'whither', 'who', 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with', 'within', 'without' , 'would', 'yet', 'yo', 'your', 'yours', 'yourself', 'yourselves'];
+var allCategories ={'Food': restaurants, 'Gas': gas, 'Grocery': grocery, 'Online Shopping': online,
+                    'Travel': travel, 'Utilities': utilities, 'Apparel': apparel, 'Entertainment': entertainment};
+
+function handleFiles(files) {
+  /* Check for the various File API support. */
+  if (window.FileReader) {
+    /* FileReader are supported. */
+    getAsText(files[0]);
+  } else {
+    alert('FileReader are not supported in this browser.');
+  }
+}
+
+function getAsText(fileToRead) {
+  var reader = new FileReader();
+  /* Handle errors load */
+  reader.onload = loadHandler;
+  reader.onerror = errorHandler;
+  /* Read file into memory as UTF-8 */
+  reader.readAsText(fileToRead);
+}
+
+function loadHandler(event) {
+  var csv = event.target.result;
+  processData(csv);
+}
+
+function processData(csv) {
+  var allTextLines = csv.split(/\r\n|\n/);
+  var lines = [];
+  var skipCount = 1; /* Skip top line */
+  var category;
+  var splitup = {};
+  lines.push(["Category", "Match", "Amount"])
+  while (allTextLines.length) {
+    allData = allTextLines.shift().split(',');
+    if(skipCount) {
+      skipCount--;
+      continue;
+    }
+    if(allData[2]) {
+      words = allData[2].split(/[\s\*\/\"\#\-\'\.]+/); category = 'MISC';
+      for(i=0;i<words.length;i++){
+        for(categoryVal in allCategories) {
+          if(words[i].toLowerCase() in allCategories[categoryVal]) {
+            category = categoryVal;
+          }
+        }
+      }
+      lines.push([category, words.join(' '), -allData[4]]);
+      if(!splitup[category])
+        splitup[category] = 0;
+      splitup[category] += (-allData[4]);
+    }
+  }
+  drawOutput(lines);
+  drawChart(splitup);
+}
+
+function errorHandler(evt) {
+  if(evt.target.error.name == "NotReadableError") {
+    alert("Cannot read file !");
+  }
+}
+
+function drawOutput(lines){
+  /* Clear previous data */
+  document.getElementById("output").innerHTML = "";
+  var table = document.createElement("table");
+  var splitup = {};
+  table.className = "table table-condensed table-hover";
+  for (i = 0; i < lines.length; i++) {
+    var row = table.insertRow(-1);
+    for (var j = 0; j < lines[i].length; j++) {
+      var moneyCell = row.insertCell(-1);
+      moneyCell.appendChild(document.createTextNode(lines[i][j]));
+    }
+  }
+  document.getElementById("output").appendChild(table);
+}
+
+function drawChart(splitup) {
+  var data = [];
+  var moneySeries = {type: "pie", showInLegend: true, toolTipContent: "{y} - #percent %", yValueFormatString: "$#.##"};
+  var money = [];
+  for(var key in splitup) {
+    money.push({y: splitup[key], legendText: key, indexLabel: key});
+  }
+  moneySeries.dataPoints = money;
+  data.push(moneySeries);
+  var chart = new CanvasJS.Chart("chartContainer",
+  {
+    theme: "theme2",
+    title:{ text: "Where your money went?" },
+    data: data
+
+  });
+  chart.render();
+}
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var files = evt.dataTransfer.files;
+    getAsText(files[0]);
+}
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+  }
+
+
+window.onload=(function () {
+  var dropZone = document.getElementById('drop_zone');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+});
